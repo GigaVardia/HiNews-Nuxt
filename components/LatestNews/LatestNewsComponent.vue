2673138f8d3ef03563091bf8a5ae6d78467a740f
GigaVardia <gigavardia@gmail.com>
2021-10-18 18:34:32 +0400

    latest news section

--- components/LatestNews/LatestNewsComponent.vue
+++ components/LatestNews/LatestNewsComponent.vue
@@ -1,22 +1,70 @@
 <template>
-  <v-carousel v-model="model">
+  <v-carousel
+    v-model="model"
+    hide-delimiter-background
+    delimiter-icon="mdi-minus"
+    min-height="600"
+  >
     <v-carousel-item
-      v-for="(color, i) in colors"
-      :key="color"
+      v-for="article in news"
+      :key="article.url"
+      dark
     >
-      <v-sheet
-        :color="color"
-        height="100%"
-        tile
-      >
+      <v-sheet height="100%">
         <v-row
+          no-gutters
           class="fill-height"
-          align="center"
-          justify="center"
         >
-          <div class="text-h2">
-            Slide {{ i + 1 }}
-          </div>
+          <v-col cols="6" class="pa-4">
+            <v-card height="100%" max-height="468">
+              <v-card-text>
+                <nuxt-link to="/">
+                  <v-img
+                    :src="article.urlToImage"
+                    :lazy-src="article.urlToImage"
+                    :alt="article.title"
+                    max-height="430"
+                    max-width="816"
+                  />
+                </nuxt-link>
+              </v-card-text>
+            </v-card>
+          </v-col>
+          <v-col cols="6" class="pa-4">
+            <v-card height="100%" max-height="468">
+              <v-card-title class="text-h4 mb-2">
+                {{ article.title }}
+              </v-card-title>
+
+              <v-card-subtitle>
+                {{ article.author }}
+              </v-card-subtitle>
+
+              <v-card-subtitle class="text-h5">
+                {{ article.description }}
+              </v-card-subtitle>
+
+              <v-card-text class="text-h5">
+                {{ article.content }}
+              </v-card-text>
+
+              <v-card-actions>
+                <a :href="article.url" target="_blank">
+                  Source
+                </a>
+                <v-btn
+                  color="blue"
+                  class="ml-auto"
+                  to="/news"
+                  nuxt
+                  text
+                  depressed
+                >
+                  Read more...
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
         </v-row>
       </v-sheet>
     </v-carousel-item>
@@ -26,15 +74,14 @@
 <script>
 export default {
   name: 'LatestNewsComponent',
+  props: {
+    news: {
+      type: Array,
+      required: true
+    }
+  },
   data: () => ({
-    model: 0,
-    colors: [
-      'primary',
-      'secondary',
-      'yellow darken-2',
-      'red',
-      'orange'
-    ]
+    model: 2
   })
 }
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,25 @@
 <template>
   <v-row>
     <v-col>
-      <latest-news-component />
+      <latest-news-component :news="latestNews" />
     </v-col>
   </v-row>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    news: null,
+    latestNewsSize: 5
+  }),
+  async fetch () {
+    this.news = await fetch(`https://newsapi.org/v2/top-headlines?country=us&category=technology&apiKey=${process.env.API_KEY}`)
+      .then(res => res.json())
+  },
+  computed: {
+    latestNews () {
+      return this.news.articles.slice(0, this.latestNewsSize)
+    }
+  }
+}
+</script>
